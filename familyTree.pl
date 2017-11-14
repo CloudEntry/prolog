2ee9dd7062c10736f3eaec7211865ca0b1e3a94f
Jack Gee <u6jg1@intra.csc.liv.ac.uk>
2017-11-14 17:27:32 +0000

    Solved problem of being your own sibling with negation as failure

--- familyTree.pl
+++ familyTree.pl
@@ -39,18 +39,19 @@ daughter(X,Y):-parent(Y,X),female(X).
 grandparent(X,Z):-parent(X,Y),parent(Y,Z).
 grandmother(X,Z):-mother(X,Y),parent(Y,Z).
 grandfather(X,Z):-father(X,Y),parent(Y,Z).
-sibling(X,Y):-parent(Z,X),parent(Y,Z).
-sister(X,Y):-female(X),parent(Z,X),parent(Z,Y).
-brother(X,Y):-male(X),parent(Z,X),parent(Z,Y).
+sibling(X,Y):-parent(Z,X),parent(Z,Y),different(X,Y).
+different(X,X):-!,fail.
+different(_._).
+sister(X,Y):-female(X),sibling(X,Y).
+brother(X,Y):-male(X),sibling(X,Y).
 aunt(X,Z):-sister(X,Y),parent(Y,Z).
 uncle(X,Z):-brother(X,Y),parent(Y,Z).
-cousin(X,Y):-parent(A,X),parent(B,Y),A\=B,sibling(A,B).
+cousin(X,Y):-parent(Z,X),parent(U,Y),sibling(Z,U).
 ancestor(X,Z):-parent(X,Z).
 ancestor(X,Z):-parent(X,Y),ancestor(Y,Z).
-silly(X):-silly(X).
 predecessor(X,Z):-predecessor(Y,Z),parent(X,Y).
 predecessor(X,Z):-parent(X,Z).
-predecessor2(X,Z):-parent(X,Y),predecessor2(Y,Z),
+predecessor2(X,Z):-parent(X,Y),predecessor2(Y,Z).
 predecessor2(X,Z):-parent(X,Z).
 predecessor3(X,Z):-parent(X,Z).
 predecessor3(X,Z):-predecessor3(Y,Z), parent(X,Y).
